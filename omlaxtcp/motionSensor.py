a71ade307fc6f3679dc4f4b53256bd4131bea861
Aadam <aadam1999@gmail.com>
2020-10-30 22:21:21 +0200

    Completed Functionality.

--- omlaxtcp/motionSensor.py
+++ omlaxtcp/motionSensor.py
@@ -1,64 +1,183 @@
 import RPi.GPIO as GPIO
+# Allows for the implementation of the onboard RPi GPIO pins.
 import time
+# Used to generate delay.
 import TCP
+# Allows for the transmission of relevant messages via TCP.
 import tempSensor
 
+global buzzerPin
+buzzerPin = 4
+# Default GPIO pin for buzzer
+
+
 def setup(motion):
-        GPIO.setmode(GPIO.BCM)
-	GPIO.setup(motion, GPIO.IN)
+    """
+    Configures motion sensor for input in accordance to the GPIO pin provided.
+
+    Parameters:
+    motion(int): Motion sensor pin
+    """
+    GPIO.setmode(GPIO.BCM)
+    # Sets pin numbering system
+    GPIO.setup(motion, GPIO.IN)
+    # Configures given pin for input usage.
+
 
 def motion_detect(motion):
-	if (GPIO.input(motion) == 1):
-		print "Motion detected"
-	else:
-		print "No motion detected"
+    """
+    Determines if an individual triggers a motion sensor.
+
+    Parameters:
+    motion(int): Second motion sensor pin
+    """
+    if (GPIO.input(motion) == 1):
+        print "Motion detected"
+    else:
+        print "No motion detected"
+
 
 def direc_detect(motion_out, motion_in):
-	if (GPIO.input(motion_out) == 1):
-		init_trig(motion_in)
-		ensure_low(motion_out, motion_in)
+    """
+    Determines the direction in which an individual is travelling,
+    by assessing the sequence of triggers between two motion sensors.
 
-	if (GPIO.input(motion_in) == 1):
-		init_trig(motion_out)
-		ensure_low(motion_out, motion_in)
+    Parameters:
+    motion_out(int): Outside motion sensor pin
+    motion_in(int): Inside motion sensor pin
+    """
+    if (GPIO.input(motion_out) == 1):
+        # Determines if outside motion sensor is triggered first.
+        init_trig(motion_in)
+        # Wait atleast 4 seconds for the corresponding sensor to trigger.
+        ensure_low(motion_out, motion_in)
+        # Ensure that both motion sensors are low/are not triggered.
+
+    if (GPIO.input(motion_in) == 1):
+        # Determines if inside motion sensor is triggered first.
+        init_trig(motion_out)
+        # Wait atleast 4 seconds for the corresponding sensor to trigger.
+        ensure_low(motion_out, motion_in)
+        # Ensure that both motion sensors are low/are not triggered.
+
+
+def speed_detect(motion_first, motion_second, distance):
+    """
+    Determines the direction in which an individual is travelling,
+    by assessing the time between the triggering of two motion sensors.
+
+    Parameters:
+    motion_out(int): Outside motion sensor pin
+    motion_in(int): Inside motion sensor pin
+    """
+    if (GPIO.input(motion_first) == 1):
+        # Determines if first motion sensor is triggered first.
+        speed(timing(motion_second), distance)
+        # Sends speed as message to server when corresponding sensor triggered.
+        ensure_low(motion_first, motion_second)
+        # Ensure that both motion sensors are low/are not triggered.
+
+    if (GPIO.input(motion_second) == 1):
+        # Determines if second motion sensor is triggered first.
+        speed(timing(motion_first), distance)
+        # Sends speed as message to server when corresponding sensor triggered.
+        ensure_low(motion_first, motion_second)
+        # Ensure that both motion sensors are low/are not triggered.
+
 
 def init_trig(motion):
-	for x in range(40):
-		time.sleep(0.1)
-		if (GPIO.input(motion) == 1):
-			if (motion == 1) and (tempSensor.scanned == False):
-				GPIO.setup(4, GPIO.OUT)
-				GPIO.output(4, GPIO.HIGH)
-				time.sleep(2)
-				GPIO.output(4, GPIO.LOW)
-				print("BUZZ")
-				break
-			else:
-				TCP.client(str(motion))
-				tempSensor.scanned = False
-				break
+    """
+    Waits up to 4 seconds until corresponding sensor is triggered.
+    If sensor triggered and temperature sensor not been triggered prior,
+    buzzer will go off.
+    If had triggered prior, direction of movement sent as message to server.
 
-def speed_detect(motion_out, motion_in, distance):
-		if (GPIO.input(motion_out) == 1):
-			speed(timing(motion_in), distance)
-			ensure_low(motion_out, motion_in)
+    Parameters:
+    motion(int): Second motion sensor pin
+    """
+    for x in range(40):
+        # Loop for 40 iterations.
+        time.sleep(0.1)
+        # Delay for 10ms.
+        if (GPIO.input(motion) == 1):
+            # Determines if corresponding motion sensor is triggered.
+            if (motion == 1) and (!tempSensor.scanned):
+                # Determines if user enters without temp scan.
+                GPIO.setup(buzzerPin, GPIO.OUT)
+                # Set buzzerPin to output mode.
+                GPIO.output(buzzerPin, GPIO.HIGH)
+                # Set buzzerPin high.
+                time.sleep(2)
+                # Delay for 2 seconds.
+                GPIO.output(buzzerPin, GPIO.LOW)
+                # Set buzzerPin low.
+                print("BUZZ")
+                break
+            else:
+                TCP.client(str(motion))
+                # Sends direction of motion as message to the connected server.
+                tempSensor.scanned = False
+                break
 
-		if (GPIO.input(motion_in) == 1):
-			speed(timing(motion_out), distance)
-			ensure_low(motion_out, motion_in)
 
 def timing(motion):
-	tic = time.perf_counter()
-	while (GPIO.input(motion) != 1):
-		pass
+    """
+    Returns calculated time between triggering of corresponding motion sensor
+    after the first motion sensor is triggered. Accurate to 10ms.
 
-	toc = time.perf_counter()
-	return tic - toc
+    Parameters:
+    motion(int): Second motion sensor pin
+
+    Returns:
+    tic - toc (float): Length of time in seconds
+    """
+    tic = time.perf_counter()
+    # Establishes time in which first motion sensor is triggered.
+    while (GPIO.input(motion) != 1):
+        # Determines if corresponding motion sensor is triggered.
+        time.sleep(0.01)
+        # Delay by 10ms.
+
+    toc = time.perf_counter()
+    # Establishes time in which second motion sensor is triggered.
+    return tic - toc
+
 
 def speed(time, distance):
-	speed = distance/time
-	TCP.client(str(speed))
+    """
+    Calculates and sends speed as a message to server.
+
+    Parameters:
+    time(float): Time given in seconds
+    distance(float): Distance given in meters
+    """
+    speed = distance/time
+    # Calculates speed in meters/second.
+    TCP.client(str(speed))
+    # Sends speed as a message to the connected server.
+
 
 def ensure_low(motion_out, motion_in):
-	while (GPIO.input(motion_out) == 1) or (GPIO.input(motion_in) == 1):
-		time.sleep(0.1)
+    """
+    Returns only when both motion sensors are low/are not triggered.
+
+    Parameters:
+    motion_out(int): Outside motion sensor pin
+    motion_in(int): Inside motion sensor pin
+    """
+    while (GPIO.input(motion_out) == 1) or (GPIO.input(motion_in) == 1):
+        # Determines if either of the two motion sensors are high/triggered.
+        time.sleep(0.1)
+        # Delay by 100ms.
+
+
+def setBuzzerPin(Pin):
+    """
+    Sets the Buzzer GPIO Pin to desired Pin value
+
+    Parameters:
+    Pin(int): A pin number
+    """
+    global buzzerPin
+    buzzerPin = Pin
+    # Replaces old pin value with the new Pin argument.
--- omlaxtcp/tempSensor.py
+++ omlaxtcp/tempSensor.py
@@ -4,57 +4,98 @@ import time
 import crcmod.predefined
 import TCP
 
+global scanned, init
 scanned = False
 init = False
 
+
 def setup(channel, omAddress, arraysize):
-	global BUFFER_LENGTH
-	global tdata_raw
-	global handle
-	global tdata
-	global pi
-	BUFFER_LENGTH =  (arraysize *2 ) + 3
-	pi = pigpio.pi()
-	piGPIO = pi.get_pigpio_version()
-	bus = smbus2.SMBus(channel)
-	time.sleep(0.1)
-	handle = pi.i2c_open(channel, omAddress)
-	tdata_raw = [0]*BUFFER_LENGTH
-	tdata = [0.0]*arraysize
+    """
+    Enables I2C communication between Raspberry Pi and the temperature sensor.
+
+    Parameters:
+    channel(int): Raspberry Pi i2c channel
+    omAddress(int): Omron I2C address
+    arraysize(int): Size of thermal array
+    """
+    global BUFFER_LENGTH
+    global tdata_raw
+    global handle
+    global tdata
+    global pi
+    BUFFER_LENGTH = (arraysize*2) + 3
+    # Establish data buffer size.
+    pi = pigpio.pi()
+    bus = smbus2.SMBus(channel)
+    # Create a bus for I2C communication.
+    time.sleep(0.1)
+    # Delay for 10ms.
+    handle = pi.i2c_open(channel, omAddress)
+    tdata_raw = [0]*BUFFER_LENGTH
+    tdata = [0.0]*arraysize
+
 
 def feverScanner():
-	global init
-	global scanned
-	tempReading()
-	if (tdata[0] > roomtemp) and (tdata[0] > 30):
-		if (init == False):
-			init = True
-			feverScanner()
-		else:
-			init = False
-			scanned = True
-			TCP.client(str(tdata[0]))
-			if (tdata[0]) > 38:
-                                scanned = False
-				time.sleep(1)
-				
+    """
+    Determines if an individual is standing infront of the temperature sensor,
+    waits a full second and reads the temperature value again to gain a more
+    stable value. Also determines if temperature is above that of the norm.
+    This data is relayed as a message to the server.
+    """
+    global init
+    global scanned
+    tempReading()
+    if (tdata[0] > roomtemp) and (tdata[0] > 30):
+        if (!init):
+            init = True
+            feverScanner()
+        else:
+            init = False
+            scanned = True
+            TCP.client(str(tdata[0]))
+            if (tdata[0]) > 38:
+                scanned = False
+                time.sleep(1)
+
+
 def tempReading():
-        global tdata
-        time.sleep(1)
-  	(bytes_read, tdata_raw) = pi.i2c_read_device(handle, BUFFER_LENGTH)
-   	a = 0
-   	b = len(tdata_raw)-2
-   	for i in range(2,b,2):
-      		tdata[a]=float((tdata_raw[i+1]<<8) | tdata_raw[i])/10
-      		a += 1
+    """
+    Determines the temperature of an object in front of the temperature sensor.
+    """
+    global tdata
+    time.sleep(1)
+    (bytes_read, tdata_raw) = pi.i2c_read_device(handle, BUFFER_LENGTH)
+    a = 0
+    b = len(tdata_raw)-2
+    for i in range(2, b, 2):
+        tdata[a] = float((tdata_raw[i+1] << 8) | tdata_raw[i])/10
+        a += 1
+
 
 def roomtemp():
-        global roomtemp
-        tPAT = float((tdata_raw[1]<<8)|tdata_raw[0])/10
-   	roomtemp = tPAT
-   	
-def roomtempValue():
-        return(roomtemp)
+    """
+    Determines the room temperature.
+    """
+    global roomtemp
+    tPAT = float((tdata_raw[1] << 8) | tdata_raw[0])/10
+    roomtemp = tPAT
 
-def tempReadingValue():
-        return(tdata[0])
+
+def getroomtempValue():
+    """
+    Retreives the most recently determined room temperature reading.
+
+    Returns:
+    roomtemp(float): Room temperature in degrees celsius
+    """
+    return(roomtemp)
+
+
+def gettempReadingValue():
+    """
+    Retreives the most recently determined temperature reading.
+
+    Returns:
+    tdata[0](float): Temperature in degrees celsius
+    """
+    return(tdata[0])
